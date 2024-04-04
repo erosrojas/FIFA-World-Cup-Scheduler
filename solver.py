62cbb3def2645dced0ad93b59518bcde5c9456a2
Siddharth Nand <snand233@gmail.com>
2024-04-04 16:20:18 -0700

    2 games per stadium in total

--- solver.py
+++ solver.py
@@ -9,12 +9,12 @@ T = 4  # Number of teams
 S = 6  # Number of stadiums
 D = 24  # Number of days
 
-teams = [f"t{i}" for i in range(T)]
+teams = [i for i in range(T)]
 
 t1s = teams.copy()
 t2s = teams.copy()
-stadiums = [f"s{i}" for i in range(S)]
-days = [f"d{i}" for i in range(D)]
+stadiums = [i for i in range(S)]
+days = [i for i in range(D)]
 
 model = cp_model.CpModel()
 
@@ -35,29 +35,35 @@ for t1 in teams:
             m1 = [schedule[f"{t2}_{t1}_{d}_{s}"] for d in days for s in stadiums]
             model.Add(sum(m1) + sum(m2) <= 1)
 
-# teams can't play against themselves
-for team in teams:
-  for day in days:
-    matches_on_day = [schedule[f"{team}_{team}_{day}_{s}"] for s in stadiums]
-    model.Add(sum(matches_on_day) == 0)
-
 # teams can only play once per day
 for team in teams:
   for day in days:
     matches_on_day = [schedule[f"{team}_{t2}_{day}_{s}"] for t2 in teams for s in stadiums]
     model.Add(sum(matches_on_day) <= 1)
 
-# one game per stadium per day
-for d in days:
-    for s in stadiums:
-        matches_on_day_stadium = [schedule[f"{t1}_{t2}_{d}_{s}"] for t1 in teams for t2 in teams if t1 != t2]
-        model.Add(sum(matches_on_day_stadium) <= 1)
+# 2 games per stadium in total
+for day in range(D):
+    for stadium in range(S):
+        matches_per_stadium = sum(
+           schedule[f"{team1}_{team2}_{day}_{stadium}"] for team1 in range(T) for team2 in range(T) if team1 != team2 for day in range(D)
+        )
+        model.Add(matches_per_stadium <= 2)
 
-# each team plays a given stadium at most twice
+# each team plays a given stadium at most once
 for team in teams:
     for s in stadiums:
         matches_in_stadium = [schedule[f"{team}_{t2}_{d}_{s}"] for t2 in teams for d in days]
-        model.Add(sum(matches_in_stadium) <= 2)
+        model.Add(sum(matches_in_stadium) <= 1)
+
+# ensure 3 day break between games
+for day in range(D - 3):
+    for stadium in range(S):
+        for team in range(T):
+            plays_today_or_next_three = sum(
+                schedule[f"{team}_{other_team}_{day + i}_{stadium}"] + schedule[f"{other_team}_{team}_{day + i}_{stadium}"] 
+                for i in range(4) for other_team in range(T) if other_team != team)
+
+            model.Add(plays_today_or_next_three <= 1)
 
 # ------------------ REVENUE FUNCTION ------------------
 
@@ -65,14 +71,20 @@ std_cap = pd.read_excel("stadium_capacity.xlsx", index_col=0, sheet_name="Sheet1
 std_cap = std_cap[std_cap['Region'] == 'Central']
 std_cap.index = stadiums
 
-def team_index(s):
-    t1 = int(s.split('_')[0][-1])
-    t2 = int(s.split('_')[1][-1])
+def index(s):
+    components = s.split('_')
+    
+    for i in range(len(components)):
+        components[i] = int(components[i])
+    
+    return components
 
+def team_rank(index):
+    t1, t2 = index[0], index[1]
     return (t1 + t2) / 2
 
 revenue = sum(
-    std_cap.loc[stadium, 'Capacity'] * schedule[f"{t1}_{t2}_{d}_{stadium}"] * team_index(f"{t1}_{t2}") for t1 in teams for t2 in teams if t1 != t2 for d in days for stadium in stadiums
+    std_cap.loc[stadium, 'Capacity'] * schedule[f"{t1}_{t2}_{d}_{stadium}"] * team_rank(index(f"{t1}_{t2}")) for t1 in teams for t2 in teams if t1 != t2 for d in days for stadium in stadiums
 )
 
 model.Maximize(revenue)
@@ -84,11 +96,11 @@ status = solver.Solve(model)
 
 if status == cp_model.OPTIMAL:
     print("Solution:")
-    for team in t1s:
-        for team2 in t2s:
-            for day in days:
-                for stadium in stadiums:
+    for day in days:
+        for stadium in stadiums:
+            for team in t1s:
+                for team2 in t2s:
                     if solver.Value(schedule[f"{team}_{team2}_{day}_{stadium}"]):
-                        print(f"{team} vs {team2} on {day} at {stadium}")
+                        print(f"Day {day}: Team {team} vs Team {team2} at Stadium {stadium}")
 else:
     print("No solution found.")
